2349f01d38763e3034b409f6eefe9351e9695a93
kokomimainr <akmatovamantur097@gmail.com>
2024-09-12 14:21:10 +0300

    feat: AuthClient

--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -1,8 +1,10 @@
-const router = require('express').Router()
+const router = require("express").Router();
 const authRouter = require("./auth.routes");
-const animalsRouter = require('./animals.routes')
+const animalsRouter = require("./animals.routes");
+const tokensRouter = require("./token.routes");
 
 router.use("/auth", authRouter);
-router.use('/animals',  animalsRouter)
+router.use("/animals", animalsRouter);
+router.use("/tokens", tokensRouter);
 
-module.exports = router
+module.exports = router;
